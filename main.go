1395f06d5c6c93df6a55b1941ac86084b672810d
Juan Cuzmar <juan.cuzmar.s@gmail.com>
2019-12-06 20:39:05 -0300

    Handle wasm_exec

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -28,6 +32,7 @@ var (
 	listenAddr string
 	healthy    int32
 	directory  string
+	once       sync.Once
 )
 
 func main() {
@@ -35,7 +40,7 @@ func main() {
 	flag.StringVar(&directory, "d", "public", "the directory of static file to host")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger := GetLogger()
 	logger.Println("Server is starting...")
 
 	router := http.NewServeMux()
@@ -43,6 +48,7 @@ func main() {
 	router.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	router.Handle("/", fileServer)
 	router.Handle("/healthz", healthz())
+	GettingWasmJS()
 
 	nextRequestID := func() string {
 		return fmt.Sprintf("%d", time.Now().UnixNano())
@@ -85,6 +91,89 @@ func main() {
 	<-done
 	logger.Println("Server stopped")
 }
+
+// GettingWasmJS grab file
+func GettingWasmJS() {
+	path, _ := os.Getwd()
+	// https://golangcode.com/cross-platform-file-paths/
+	file := path + filepath.FromSlash("/public/wasm_exec.js")
+	// /misc/wasm/wasm_exec.js
+	wasm := runtime.GOROOT() + filepath.FromSlash("/misc/wasm/wasm_exec.js")
+	if _, err := os.Stat(file); err == nil {
+		return
+	}
+	err := CopyFile(wasm, file)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func CopyFile(src, dst string) (err error) {
+	sfi, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	if !sfi.Mode().IsRegular() {
+		// cannot copy non-regular files (e.g., directories,
+		// symlinks, devices, etc.)
+		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+	}
+	dfi, err := os.Stat(dst)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return
+		}
+	} else {
+		if !(dfi.Mode().IsRegular()) {
+			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+		}
+		if os.SameFile(sfi, dfi) {
+			return
+		}
+	}
+	if err = os.Link(src, dst); err == nil {
+		return
+	}
+	err = copyFileContents(src, dst)
+	return
+}
+
+// copyFileContents copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file.
+func copyFileContents(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		return
+	}
+	err = out.Sync()
+	return
+}
+
+// GetLogger create new logger
+func GetLogger() *log.Logger {
+	var logger *log.Logger
+	once.Do(func() {
+		logger = log.New(os.Stdout, "http: ", log.LstdFlags)
+	})
+	return logger
+}
+
 func serveIndex() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./index.html")
@@ -145,7 +234,6 @@ func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
 
 // Open opens file
 func (fs FileSystem) Open(path string) (http.File, error) {
-	fmt.Println(path)
 	f, err := fs.fs.Open(path)
 	if err != nil {
 		return nil, err
